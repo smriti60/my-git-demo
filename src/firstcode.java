dd4df8e8f8d3f0fd9de397203a544c3e42306f6c
smriti60 <smritichoudhury1901@gmail.com>
2022-05-27 21:01:34 +0530

    first branch 'iss1' commit

--- src/firstcode.java
+++ src/firstcode.java
@@ -1,8 +1,13 @@
+import com.cts.service.SomeService;
 
 public class firstcode {
 
 	public static void main(String[] args) {
 		System.out.println("first code to commit");
+		
+		
+		SomeService ss=new SomeService();
+		System.out.println(ss.findLength("program"));
 
 	}
 
